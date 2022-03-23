ee1cf2ceb0092b6f31b3fb2ad824f6d1344bb687
doziestar <chidosiky2015@gmail.com>
2022-03-23 08:31:35 +0100

    setting service in udrems core

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,72 +1,44 @@
-// // import userModel from '@models/users.model';
-// import { User } from '@/entity/User.entity';
-// import { CreateUserDto } from '@dtos/users.dto';
-// import { HttpException } from '@exceptions/HttpException';
-// import { DataStoredInToken, TokenData } from '@interfaces/auth.interface';
-// import { IUser } from '@interfaces/users.interface';
-// import { isEmpty } from '@utils/util';
-// import bcrypt from 'bcrypt';
-// import config from 'config';
-// import jwt from 'jsonwebtoken';
+/**
+ * @author Chidozie C. Okafor
+ * @description This is authentication repository for user authentication
+ * @param {type} type
+ */
+import { CreateUserDto } from '@dtos/users.dto';
+import { User } from '@entity/User.entity';
+import { IAuthRepository } from '@interfaces/Authrepo.interface';
+import { IUser } from '@interfaces/users.interface';
+import { getRepository, Repository } from 'typeorm';
 
-// class AuthService {
-//   public users = User;
+class AuthRepository implements IAuthRepository {
+  public async signup(createUserDto: CreateUserDto): Promise<IUser> {
+    const userRepository: Repository<User> = getRepository(User);
+    const userExist = await userRepository.findOne({ where: { email: createUserDto.email } });
+    console.log(userExist);
+    if (userExist) {
+      throw new Error('User already exist');
+    }
+    const user = await userRepository.create(createUserDto);
+    const savedUser = await userRepository.save(user);
+    return savedUser;
+  }
 
-//   // public async validateUser(payload: DataStoredInToken): Promise<IUser> {
-//   //   const user = await this.users.
-//   //   if (!user) {
-//   //     throw new HttpException(401, 'Invalid credentials');
-//   //   }
-//   //   return user;
-//   // }
+  public async login(loginUserDto: any): Promise<{ user: IUser; token: string }> {
+    const userRepository: Repository<User> = getRepository(User);
+    const user = await userRepository.findOne({
+      where: { email: loginUserDto.email },
+    });
+    if (user) {
+      const passwordMatch = await user.comparePassword(loginUserDto.password);
+      if (passwordMatch) {
+        const token = await user.generateToken();
+        return {
+          user,
+          token,
+        };
+      }
+    }
+    throw new Error('Invalid credentials');
+  }
+}
 
-//   public async signup(userData: CreateUserDto): Promise<IUser> {
-//     if (isEmpty(userData)) throw new HttpException(400, "You're not userData");
-
-//     const findUser: IUser = this.users.find(user => user.email === userData.email);
-//     if (findUser) throw new HttpException(409, `You're email ${userData.email} already exists`);
-
-//     const hashedPassword = await bcrypt.hash(userData.password, 10);
-//     const createUserData: IUser = { id: this.users.length + 1, ...userData, password: hashedPassword };
-
-//     return createUserData;
-//   }
-
-//   public async login(userData: CreateUserDto): Promise<{ cookie: string; findUser: IUser }> {
-//     if (isEmpty(userData)) throw new HttpException(400, "You're not userData");
-
-//     const findUser: IUser = this.users.find(user => user.email === userData.email);
-//     if (!findUser) throw new HttpException(409, `You're email ${userData.email} not found`);
-
-//     const isPasswordMatching: boolean = await bcrypt.compare(userData.password, findUser.password);
-//     if (!isPasswordMatching) throw new HttpException(409, "You're password not matching");
-
-//     const tokenData = this.createToken(findUser);
-//     const cookie = this.createCookie(tokenData);
-
-//     return { cookie, findUser };
-//   }
-
-//   public async logout(userData: IUser): Promise<IUser> {
-//     if (isEmpty(userData)) throw new HttpException(400, "You're not userData");
-
-//     const findUser: IUser = this.users.find(user => user.email === userData.email && user.password === userData.password);
-//     if (!findUser) throw new HttpException(409, "You're not user");
-
-//     return findUser;
-//   }
-
-//   public createToken(user: IUser): TokenData {
-//     const dataStoredInToken: DataStoredInToken = { id: user.id };
-//     const secretKey: string = config.get('secretKey');
-//     const expiresIn: number = 60 * 60;
-
-//     return { expiresIn, token: jwt.sign(dataStoredInToken, secretKey, { expiresIn }) };
-//   }
-
-//   public createCookie(tokenData: TokenData): string {
-//     return `Authorization=${tokenData.token}; HttpOnly; Max-Age=${tokenData.expiresIn};`;
-//   }
-// }
-
-// export default AuthService;
+export default AuthRepository;
