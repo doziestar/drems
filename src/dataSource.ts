230bb090403aace9782c72077561d02292140341
doziestar <chidosiky2015@gmail.com>
2022-03-25 07:00:34 +0100

    working on faceId and finger printing

--- src/dataSource.ts
+++ src/dataSource.ts
@@ -1,13 +1,17 @@
 import { DataSource } from 'typeorm';
+import * as PostgressConnectionStringParser from 'pg-connection-string';
+
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/udrems';
+const connectionOptions = PostgressConnectionStringParser.parse(connectionString);
 
 export const UdremsData = new DataSource({
-  name: 'default',
+  name: 'udrems',
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: '918273645dozie',
-  database: 'udrems',
+  host: connectionOptions.host,
+  port: parseInt(connectionOptions.port),
+  username: connectionOptions.user,
+  password: connectionOptions.password,
+  database: connectionOptions.database,
   logging: true,
   synchronize: true,
   entities: ['src/entity/**/*.ts'],
